a2169afa6a9438ec98908fc14fa6267e6231c0a3
rodrigo-delgado <rod.delgado85@gmail.com>
2017-07-12 16:22:04 +1200

    sort players by deacresing score

--- client/main.js
+++ client/main.js
@@ -10,7 +10,12 @@ import App from './../imports/ui/App';
 Meteor.startup(() => {
   Tracker.autorun(() => {
 
-    let players = Players.find().fetch();
+    let players = Players.find({},{
+        sort: {
+          score: -1
+        }
+      }
+    ).fetch();
     let title = 'Score Keep';
     let subtitle = 'Created by RGDV'
 
